a85cd9dc4865409272aedb95e2d2dcd871323581
margaritayong <lyong@sva.edu>
2017-10-05 19:17:34 -0400

    Examples
    Examples added to Week 2 folder

---
+++ week_02/examples/boolean_type.py
@@ -0,0 +1,43 @@
+myBool = True
+print myBool
+
+myString = "Hello World"
+print myString
+
+word1 = "hello "
+word2 = "world"
+print (word1 + word2 + " " + myString)
+
+myInt = 1
+print myInt
+
+myFloat = 3.14
+print myFloat
+
+myInt1 = 10
+myInt2 = 20
+print (myInt1 + myInt2)
+print (1 + 3)
+print (myInt1 + 3)
+
+myList = ["Water", "Apple", "Banana"]
+print myList
+print myList[0] #this will print Water
+
+myDict = {
+	"fuel_type": "electric",
+	"model": "S", 
+	"color": "red"
+}
+print myDict
+print myDict["color"]
+
+myString1 = "1";
+myString2 = "2"
+myNum = int(myString1 + myString2)
+print myNum
+
+myNum1 = 1;
+myNum2 = 2;
+myString = str(myNum1 + myNum2)
+print myString
---
+++ week_02/examples/object_type.py
@@ -0,0 +1,8 @@
+imacObj = {
+	"screenSize": "HD",
+	"screenHeight": 1080,
+	"ScreenWidth": 1920,
+	"color": "Space Grey"
+}
+print imacObj
+print imacObj["screenHeight"]
